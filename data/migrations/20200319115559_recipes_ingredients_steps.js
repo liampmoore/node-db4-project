62a2a0fe13bff0ab5477b5e855b1d5eb50640af6
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-19 12:03:21 -0400

    Wrote all table up function except recipe_ingredients table

--- data/migrations/20200319115559_recipes_ingredients_steps.js
+++ data/migrations/20200319115559_recipes_ingredients_steps.js
@@ -1,6 +1,34 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('recipes', tbl => {
+      tbl.increments();
+      tbl.text('recipe_name', 128)
+        .notNullable();
+    })
+    .createTable('steps', tbl => {
+      tbl.increments();
+      tbl.integer('step_number')
+        .unsigned()
+        .notNullable();
+      tbl.text('instructions')
+        .notNullable();
+      tbl.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments()
+      tbl.text('ingredient_name')
+        .notNullable()
+    .createTable('recipe_ingredients', tbl => {
+      
+    })
+    })
 };
 
 exports.down = function(knex) {
